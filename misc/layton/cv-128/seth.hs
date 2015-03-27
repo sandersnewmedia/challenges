f4d41a6f4ad40810ef4b13c7707d97bfd6726cb1
Seth Root <sethElijah@gmail.com>
2015-03-26 22:26:39 -0500

    improves comments

--- misc/layton/cv-128/seth.hs
+++ misc/layton/cv-128/seth.hs
@@ -2,17 +2,17 @@ import Data.List (product, union)
 
 type Key = ([Integer], [Integer], [Integer])
 
--- generate all Integer triples e.g [[0,0,0],[0,0,1],[0,0,2], etc..]
+-- generate all unique Integer triples e.g [[0,1,2],[0,1,3],[0,1,4], etc..]
 triples :: [[Integer]]
 triples = [[x, y, z] | x <- [1..9], y <- [1..9], z <- [1..9],
-                       x /= y, y /= z, x /= z]
+                       x /= y, y /= z, x /= z] -- ensures unique Integers in triple
 
--- generate all keys
+-- generate all valid keys
 keys :: [Key]
 keys = [(x, y, z) | x <- triples, y <- triples, z <- triples,
-                    length(foldl union [] [x,y,z]) == 7, -- each number used once, two are shared
+                    length(foldl union [] [x,y,z]) == 7, -- ensure each number used once, two are shared
                     product x == product y, product y == product z] -- transitive product equality
 
--- grabs the first answer
+-- lazily evaluates to the first valid Key
 answer :: Key
 answer = head keys
