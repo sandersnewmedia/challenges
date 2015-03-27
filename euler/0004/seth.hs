8a1d1ce7397a999540a7c12b4423e13e67fa02f4
Seth Root <seth@fultonworks.co>
2015-03-26 23:58:08 -0500

    adds solution for euler 0004

---
+++ euler/0004/seth.hs
@@ -0,0 +1,14 @@
+import Data.List (sort)
+
+palindrome :: (Eq a) => [a] -> Bool
+palindrome n = reverse n == n
+
+digits3 :: [Integer]
+digits3 = [1..999]
+
+palindromes3 :: [Integer]
+palindromes3 = [a * b | a <- digits3, b <- digits3,
+                palindrome $ show $ a * b]
+
+answer :: Integer
+answer = last $ sort palindromes3
