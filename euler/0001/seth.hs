5aa58e190a9977c4ec2577d3ca0e1b88e181cd45
Seth Root <seth@fultonworks.co>
2015-03-26 23:15:17 -0500

    removes function application operator

--- euler/0001/seth.hs
+++ euler/0001/seth.hs
@@ -4,5 +4,5 @@ factor :: (Integral a) => a -> a -> Bool
 factor n = (== 0) . (mod n)
 
 answer :: Integer
-answer = sum $ [a | a <- [1..999],
-                    factor a 3 || factor a 5]
+answer = sum [a | a <- [1..999],
+                  factor a 3 || factor a 5]
