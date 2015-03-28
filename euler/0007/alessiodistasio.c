a1e7d056d2be644e6fa7adbc52ad4a159b3d4385
Alessio Di Stasio <aledista@gmail.com>
2015-03-28 02:10:28 -0500

    euler 0007 in C

---
+++ euler/0007/alessiodistasio.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int position = 10001;
+
+int prime(int number) {
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0 && i != number)
+      return 0;
+  }
+  return 1;
+}
+
+int main() {
+  int i = 0, j = 1;  
+  while (i < position) {
+    j++;
+    if (prime(j))
+      i++;
+  }
+  printf("The %dth prime is %d\r\n", position, j);
+}
+
+/*
+Compile: gcc alessiodistasio.c -o alessiodistasio
+Run: ./alessiodistasio
+*/
