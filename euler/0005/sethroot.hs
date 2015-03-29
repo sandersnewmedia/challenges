9f3749e1a8bbdcfcbb456e1950c48cd0d99c0e3f
Seth Root <seth@fultonworks.co>
2015-03-29 18:58:44 -0500

    using filter for factorsFromRange

--- euler/0005/sethroot.hs
+++ euler/0005/sethroot.hs
@@ -8,14 +8,14 @@ isPrime :: Int -> Bool
 isPrime n = not . getAny . mconcat . map Any $ [isFactor n a | a <- [2 .. (n - 1)]]
 
 rangeLimit :: Int
-rangeLimit = 20
+rangeLimit = 10
 
 range :: [Int]
 range = enumFromTo 1 rangeLimit
 
--- evaluates to all the numbers in range r that are factors of n
+-- filters range r that are factors of n
 factorsFromRange :: [Int] -> Int -> [Int]
-factorsFromRange r n = [a| a <- r, isFactor n a]
+factorsFromRange r n = filter (isFactor n) r
 
 -- smallest number to consider
 -- will contain all primes as factors as well as the range limit
