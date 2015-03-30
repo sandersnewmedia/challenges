328be16055f14420df7711b21fd5bb2d41759ddf
Alessio Di Stasio <aledista@gmail.com>
2015-03-29 20:03:08 -0500

    adds solution for euler 0005 - python

---
+++ euler/0005/alessiodistasio.py
@@ -0,0 +1,12 @@
+divisors = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+found = False
+i = divisors[-1]
+
+while not found:
+    for div in divisors:
+        if i % div != 0:
+            i += divisors[-1]
+            break
+    else:
+        found = True
+print i
