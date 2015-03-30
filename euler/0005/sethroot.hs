a26410cb1edf0ce48d1171b91876003530b5ed93
Seth Root <seth@fultonworks.co>
2015-03-29 22:28:33 -0500

    better names

--- euler/0005/sethroot.hs
+++ euler/0005/sethroot.hs
@@ -1,14 +1,14 @@
 isFactor :: (Integral a) => a -> a -> Bool
 isFactor comp fact = comp `rem` fact == 0
 
-range :: [Int]
-range = [19,18,17,16,14,13,11]
+required_factors :: [Int]
+required_factors = [19,18,17,16,14,13,11]
 
 factorsFrom :: [Int] -> Int -> [Int]
-factorsFrom r n = takeWhile (isFactor n) r
+factorsFrom range n = takeWhile (isFactor n) range
 
 answer = head . (filter pred) $ [20, 40..]
-  where pred = (== 7) . length . factorsFrom range
+  where pred = (== 7) . length . factorsFrom required_factors
 
 main :: IO()
 main = print answer
