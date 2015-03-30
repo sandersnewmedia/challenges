a299e7a5a5dc1f5b409248e08c6d756a6ef5caa4
Alessio Di Stasio <aledista@gmail.com>
2015-03-29 21:59:22 -0500

    adds solution for euler 0001 - C

---
+++ euler/0001/alessiodistasio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i = 0, sum = 0;
+  for (i = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  printf("%d\r\n", sum);
+}
+
+/*
+Compile: gcc alessiodistasio.c -o alessiodistasio
+Run: ./alessiodistasio
+*/
