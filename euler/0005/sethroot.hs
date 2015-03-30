0fb8418f91b9244cc05d8d12ba22e71ff569b3ec
Seth Root <seth@fultonworks.co>
2015-03-29 22:20:34 -0500

    borrowing alessio's range optimization and taking advantage of laziness

--- euler/0005/sethroot.hs
+++ euler/0005/sethroot.hs
@@ -2,26 +2,19 @@ import Data.List (product, union)
 import Data.Monoid
 
 isFactor :: (Integral a) => a -> a -> Bool
-isFactor n = (== 0) . (mod n)
-
-isPrime :: Int -> Bool
-isPrime n = not . getAny . mconcat . map Any $ [isFactor n a | a <- [2 .. (n - 1)]]
-
-rangeLimit :: Int
-rangeLimit = 20
+isFactor comp fact = comp `rem` fact == 0
 
 range :: [Int]
-range = enumFromTo 1 rangeLimit
+range = [19,18,17,16,14,13,11]
 
--- filters range r that are factors of n
-factorsFromRange :: [Int] -> Int -> [Int]
-factorsFromRange r n = filter (isFactor n) r
+factorsFrom :: [Int] -> Int -> [Int]
+factorsFrom r n = takeWhile (isFactor n) r
 
--- smallest number to consider
--- will contain all primes as factors as well as the range limit
-lowBound :: Int
-lowBound = product . (union [rangeLimit]) $ [a | a <- range, isPrime a]
+answer = head . (filter pred) $ [20, 40..]
+  where pred = (== 7) . length . factorsFrom range
 
-answer :: Int
-answer = head . (filter pred) $ enumFrom lowBound
-  where pred = (== rangeLimit) . length . (factorsFromRange range)
+main :: IO()
+main = print answer
+
+-- ghc -O2 sethroot.hs
+-- ./sethroot
